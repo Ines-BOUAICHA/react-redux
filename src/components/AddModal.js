950edd1aaf2beca755b1f5de441d0549eebeb12c
Ines-123 <89464218+Ines-BOUAICHA@users.noreply.github.com>
2021-10-18 21:44:09 +0100

    Add files via upload

---
+++ src/components/AddModal.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import {Modal, Form, Button} from "react-bootstrap"
+import { useDispatch } from 'react-redux'
+const AddModal = () => {
+  const dispatch = useDispatch ()
+    const [newTask, setNewTask] =useState('')
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const addFunc = () =>{
+      if(newTask.trim()){
+      dispatch({type:"add", payload:newTask})
+      handleClose ()
+      }
+    }
+
+    return (
+        <div>
+          <Button variant="primary" onClick={handleShow}>
+          Add task
+          </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add new task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form.Group>
+    <Form.Label>new task</Form.Label>
+    <Form.Control type="text" placeholder="task" onChange={(e)=>setNewTask(e.target.value)}/>
+        </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={addFunc}>
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>  
+        </div>
+    )
+}
+
+export default AddModal
